e072f9fa9066447dd5ad304e8d83cfac2db9d652
Fabio Rodrigues de Carvalho <fabio.carvalho@castgroup.com.br>
2019-10-22 17:28:06 -0300

    inicio desafio modulo 2

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'mysql',
+  host: 'localhost',
+  username: 'root',
+  password: 'docker',
+  database: 'gympoint',
+  define: {
+    timestamp: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
